00e24a3164a61a4204b23ae86bc57bbfb384e0b2
Cayman Heng <hengcayman@gmail.com>
2021-01-20 09:44:34 -0500

    added isAuthenticated middleware to the /result route. If a user who is not logged in tries to access this route, they will be redirected to the signup page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   });
 
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the search page
     if (req.user) {
       res.redirect("/search");
     }
@@ -25,5 +25,11 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/search", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
+  
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/result", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/result.html"));
   });
 };
