73e0703db91b64cd4df46e614ded456bf42c8b09
Hustin <hustinkava@gmail.com>
2021-01-22 22:06:48 -0700

    darkmode

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -27,20 +27,20 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-const routes = require('./controllers/travelController.js');
+// const routes = require('./controllers/travelController.js');
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/signup-login-api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+db.sequelize.sync({ alter: { drop: false } }).then(() => {
+    app.listen(PORT, () => {
+        console.log(
+            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            PORT,
+            PORT
+        );
+    });
 });
 // { alter: { drop: false } }
