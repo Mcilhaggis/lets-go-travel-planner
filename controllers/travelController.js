58492ab61e9b8354cd607947ab4684ee3a50c01a
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-01-24 15:08:19 -0500

    adding itinerary api routes
    added handlebar block for activity/itinerary items

--- controllers/travelController.js
+++ controllers/travelController.js
@@ -89,7 +89,49 @@ router.get("/api/user_data", (req, res) => {
   }
 });
 
-//ITINERARY API ROUTES - to be included
+//ITINERARY API ROUTES
+
+//Route for export
+module.exports = app => {
+  // GET route for getting all of the saved itenrary items
+  app.get("/api/itinerary", (req, res) => {
+    // findAll returns all entries for a table when used with no options
+    db.Itinerary.findAll({
+      where: {
+        memberID: "test" // not test but equal to the users ID from login
+      }
+    }).then(result => res.json(result));
+  });
+
+  // POST route for saving a new itinerary item
+  app.post("/api/itinerary", (req, res) => {
+    console.log(req.body);
+    // Create takes an argument of an object describing the item we want to
+    // Insert into our table. We pass in an object with a text and complete property.
+    db.Itinerary.create({
+      memberId: "test", //this may come from a different place than the rest - referring to the user that is currently logged in
+      activityId: "test",
+      activityImageURL: "test",
+      activityName: "test",
+      activityDescription: "test",
+      restaurantName: "test",
+      restaurantDescription: "test",
+      restaurantPhoto: "test",
+      restaurantLocation: "test",
+      menuURL: "test",
+      userRating: "test"
+    }).then(result => res.json(result)); // result may not be the right name for
+  });
+};
+// Deleting a previously saved item
+app.delete("/api/itinerary/:activityId", (req, res) => {
+  // We just have to specify which itinerary item we want to destroy with "where"
+  db.Itinerary.destroy({
+    where: {
+      id: req.params.activityId // we get this value from a click on a button of the item it's attached to
+    }
+  }).then(result => res.json(result));
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/itinerary.js
+++ models/itinerary.js
@@ -2,36 +2,41 @@
 const Sequelize = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    const Itinerary = sequelize.define(
-        "Itinerary", {
-            memberId: Sequelize.INTEGER, // MemberId will be used as a foreign key with User.Id to link the two tables
-            destination: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            activityId: Sequelize.INTEGER,
-            activityName: Sequelize.STRING,
-            restaurantName: Sequelize.STRING,
-            restaurantURL: Sequelize.STRING,
-            restaurantLocation: Sequelize.STRING,
-            menuURL: Sequelize.STRING,
-            userRating: Sequelize.INTEGER,
-            restaurantPhoto: Sequelize.STRING // Photo URL
-        }, {
-            freezeTableName: true, // Model tableName will be the same as the model name
-            timestamps: false // Removing the createdAt and updatedAt defualt columns from this table
-        }
-    );
+  const Itinerary = sequelize.define(
+    "Itinerary",
+    {
+      memberId: Sequelize.INTEGER, // MemberId will be used as a foreign key with User.Id to link the two tables
+      destination: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      activityId: Sequelize.INTEGER,
+      activityImageURL: Sequelize.STRING,
+      activityName: Sequelize.STRING,
+      activityDescription: Sequelize.STRING,
+      restaurantURL: Sequelize.STRING,
+      restaurantName: Sequelize.STRING,
+      restaurantDescription: Sequelize.STRING,
+      restaurantPhoto: Sequelize.STRING, // Photo URL
+      restaurantLocation: Sequelize.STRING,
+      menuURL: Sequelize.STRING,
+      userRating: Sequelize.INTEGER
+    },
+    {
+      freezeTableName: true, // Model tableName will be the same as the model name
+      timestamps: false // Removing the createdAt and updatedAt defualt columns from this table
+    }
+  );
 
-    Itinerary.associate = models => {
-        // We're saying that a Itinerary should belong to a User
-        // A Itinerary can't be created without a User due to the foreign key constraint
-        Itinerary.belongsTo(models.User, {
-            foreignKey: {
-                memberId: Sequelize.INTEGER
-            }
-        });
-    };
+  Itinerary.associate = models => {
+    // We're saying that a Itinerary should belong to a User
+    // A Itinerary can't be created without a User due to the foreign key constraint
+    Itinerary.belongsTo(models.User, {
+      foreignKey: {
+        memberId: Sequelize.INTEGER
+      }
+    });
+  };
 
-    return Itinerary;
+  return Itinerary;
 };
