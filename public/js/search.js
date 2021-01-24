2cce8a8a8e5b43d462363a75f2e2fdbe8e70b36a
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-01-24 18:52:42 -0500

    fixing api result display on button click

--- public/js/search.js
+++ public/js/search.js
@@ -1,51 +1,37 @@
 $(document).ready(() => {
-
-
   function getActivityResultAPI(city) {
-    $.get("/api/activity", { city: city })
-    .then((data) => {
+    $.get("/api/activity", { city: city }).then((data) => {
       console.log(data);
       //put data into your page;
     });
   }
 
   function getRestaurantAPI(city) {
-    $.get("/api/restaurants", { city: city })
-    .then((data) => {
+    $.get("/api/restaurants", { city: city }).then((data) => {
       console.log(data);
       //put data into your page;
     });
   }
 
-  
+  // const testName = "New York"
+  //   getRestaurantAPI(testName);
+  //   getActivityResultAPI(testName);
 
-
-// const testName = "New York"
-//   getRestaurantAPI(testName);
-//   getActivityResultAPI(testName); 
-
-
-
-
-$("#create-form").on("submit", (event) => {
-  const cityName = ca.value.trim();
-  console.log(ca.value.trim());
-  console.log('aaaaaa');
-  getRestaurantAPI(cityName);
-  getActivityResultAPI(cityName); 
+  $("#create-form").on("submit", event => {
+    event.preventDefault();
+    const cityName = ca.value.trim();
+    console.log(ca.value.trim());
+    console.log("aaaaaa");
+    getRestaurantAPI(cityName);
+    getActivityResultAPI(cityName);
   });
 
-
-
-  
-
-
   // // When the form is submitted, we validate there's an email and password entered
-  // // searchBtn.on("click", (e) => {
-  // //   e.preventDefault();
-  // //   const cityName = ca.val().trim();
-  // //   console.log("I've been clicked");
-  // //   console.log(cityName);
+  // searchBtn.on("click", (e) => {
+  //   e.preventDefault();
+  //   const cityName = ca.val().trim();
+  //   console.log("I've been clicked");
+  //   console.log(cityName);
 
   // //   // If we have an email and password we run the loginUser function and clear the form
   // //   getRestaurantAPI('New York');
@@ -54,8 +40,4 @@ $("#create-form").on("submit", (event) => {
   // //   //Empty the search bar once search submitted
   // //   ca.val("");
   // // });
-
 });
-
-
-
