157020c6b191e65574d6cc7de099bd66dc94c995
Hustin <hustinkava@gmail.com>
2021-01-23 23:59:16 -0700

    refactored api code

---
+++ controllers/api.js
@@ -0,0 +1,65 @@
+// Requiring our models and passport as we've configured it
+const db = require("../models");
+const passport = require("../config/passport");
+const zomato = require("../api/zomato");
+const amadeus = require("../api/amadeus");
+
+module.exports = function(app) {
+    // Get Activities from Amadeus API
+    app.get("/api/activity", (req, res) => {
+        //Get geo-location
+        amadeus.getActivity(req.query.city).then(function(geocode) {
+            // Get amadeus token
+            amadeus.getTokenActivities().then(function(token) {
+                // Get amadeus Activities
+                amadeus.getActivityResult(token, geocode).then(function(activities) {
+                    let act = activities.data.slice(0, 3);
+                    // console.log(act);
+                    let allActivities = {
+                        activities: act.map((o) => [
+                            (Activity = {
+                                name: o.name,
+                                description: o.shortDescription,
+                                rating: o.rating,
+                                price: o.price.amount,
+                                photo: o.pictures[0],
+                            }),
+                        ]),
+                    };
+                    console.log(allActivities);
+                    //For Testing
+                    res.send(allActivities);
+
+                    // // ==== PREPARED FOR HANDLEBARS=====
+                    // res.render('result', {allRestaurnt});
+                });
+            });
+        });
+    });
+
+    // Call Api function from Class 'zomato'
+    app.get("/api/restaurants", (req, res) => {
+        zomato.getZomatoRestaurant(req.query.city).then(function(result) {
+            // console.log(result);
+            let allRestaurnt = {
+                restaurants: result.restaurants.map((o) => [
+                    (restaurant = {
+                        name: o.restaurant.name,
+                        url: o.restaurant.url,
+                        address: o.restaurant.location.address,
+                        rating: o.restaurant.all_reviews.rating,
+                        menu: o.restaurant.menu_url,
+                        phone: o.restaurant.phone_numbers,
+                        photos: o.restaurant.photos_url,
+                    }),
+                ]),
+            };
+
+            // ==== TESTING ON result.js ====
+            res.send(allRestaurnt);
+
+            // // ==== PREPARED FOR HANDLE_BAR=====
+            // res.render('result', {allRestaurnt});
+        });
+    });
+};
--- controllers/zomato.js
+++ controllers/zomato.js
@@ -1,56 +1,23 @@
 const fetch = require("node-fetch");
 
-
-
-// //Get Zomato City detail
-// function getZomatoCity() { 
-//   fetch('https://developers.zomato.com/api/v2.1/cities?q='+cityParam, {
-//     headers: {
-//       'user-key': '8afc96c75a3fbe2985c0d465fe2c3940'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(data => console.log(data.location_suggestions) );
-  
-// }
-
-
-
 //Get Zomato restaurant by CityId
-function getZomatoRestaurant(city) { 
-   return fetch('https://developers.zomato.com/api/v2.1/search?q='+city+'&start=1&count=3', {
-    headers: {
-      'user-key': '8afc96c75a3fbe2985c0d465fe2c3940'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    // let restaurants = data.restaurants.map((o) => o.restaurant);
-    // data.restaurants = restaurants;
-    return data;
-  });
-
-
-
-  
+function getZomatoRestaurant(city) {
+    return fetch(
+            "https://developers.zomato.com/api/v2.1/search?q=" +
+            city +
+            "&start=1&count=3", {
+                headers: {
+                    "user-key": "8afc96c75a3fbe2985c0d465fe2c3940",
+                },
+            }
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        });
 }
 
-
-
 module.exports = {
-  // getZomatoCity, 
-  getZomatoRestaurant
+    // getZomatoCity,
+    getZomatoRestaurant,
 };
-
-
-//---------------------If you need to call this function from another file--------------//
-// require this file
-const zomato = require("../controllers/zomato");
-
-// testAPI!!!!!!!!!!!!!!!!!
-  const cityParam = 'Winnipeg'
-  
-  // Call Api function from Class 'zomato'
-  zomato.getZomatoRestaurant(cityParam).then(function(result) {
-    result.restaurants.map((o) => console.log(o.restaurant.name)) 
- })
