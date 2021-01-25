8af195d43f9f1245d30172a480da31141bc1828c
Hustin <hustinkava@gmail.com>
2021-01-24 19:33:03 -0700

    installed nodemon

--- public/js/search.js
+++ public/js/search.js
@@ -1,42 +1,23 @@
 $(document).ready(() => {
-  function getActivityResultAPI(city) {
-    $.get("/api/activity", { city: city }).then((data) => {
-      console.log(data);
-      //put data into your page;
+    function getActivityResultAPI(city) {
+        $.get("/api/activity", { city: city }).then((data) => {
+            console.log(data);
+            //put data into your page;
+        });
+    }
+
+    function getRestaurantAPI(city) {
+        $.get("/api/restaurants", { city: city }).then((data) => {
+            console.log(data);
+            //put data into your page;
+        });
+    }
+
+    $("#create-form").on("submit", event => {
+        event.preventDefault();
+        const cityName = ca.value.trim();
+        console.log(ca.value.trim());
+        getRestaurantAPI(cityName);
+        getActivityResultAPI(cityName);
     });
-  }
-
-  function getRestaurantAPI(city) {
-    $.get("/api/restaurants", { city: city }).then((data) => {
-      console.log(data);
-      //put data into your page;
-    });
-  }
-
-  // const testName = "New York"
-  //   getRestaurantAPI(testName);
-  //   getActivityResultAPI(testName);
-
-  $("#create-form").on("submit", event => {
-    event.preventDefault();
-    const cityName = ca.value.trim();
-    console.log(ca.value.trim());
-    getRestaurantAPI(cityName);
-    getActivityResultAPI(cityName);
-  });
-
-  // // When the form is submitted, we validate there's an email and password entered
-  // searchBtn.on("click", (e) => {
-  //   e.preventDefault();
-  //   const cityName = ca.val().trim();
-  //   console.log("I've been clicked");
-  //   console.log(cityName);
-
-  // //   // If we have an email and password we run the loginUser function and clear the form
-  // //   getRestaurantAPI('New York');
-  // //   getActivityResultAPI('New York');
-
-  // //   //Empty the search bar once search submitted
-  // //   ca.val("");
-  // // });
 });
