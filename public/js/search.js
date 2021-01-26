dd56ad63156dfe5eb2455dc192c13afb20b925d9
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-01-25 21:54:17 -0500

    saving restuarant name to DB on save click

--- public/js/search.js
+++ public/js/search.js
@@ -1,28 +1,22 @@
 $(document).ready(() => {
-
     const restaurantsResults = document.getElementById("restaurantsResults");
     const activitiesResults = document.getElementById("activitiesResults");
-
     // let test = getUserData();
     // setTimeout(console.log(test), 1000)
     // console.log(test)
-
     // getUserData();
-
     // function getUserData() {
     //     let userId;
     //     $.get("/api/user_data").then((data) => {
     //         console.log(data)
     //         userId = data.id;
     //         console.log(userId)
-
     //     })
     //     return userId
     // };
     // async await
     // pass id through url if we cannot figure this out
     // parse url and take off id, set to variable and use that
-
     function getActivityResultAPI(city) {
         $.get("/api/activity", { city: city }).then((data) => {
             console.log(data);
@@ -31,7 +25,6 @@ $(document).ready(() => {
                 const activitiesName = document.createElement("h2");
                 activitiesResults.appendChild(activitiesName);
                 activitiesName.textContent = data.activities[i][0].name;
-
                 // view activity photo
                 const activitiesImage = document.createElement('img');
                 activitiesImage.src = data.activities[i][0].photo;
@@ -39,42 +32,36 @@ $(document).ready(() => {
                 activitiesImage.style.height = "auto";
                 activitiesImage.style.width = "200px";
                 activitiesImage.style.float = "left";
-
                 //view activity description
                 const activitiesDescription = document.createElement('p');
                 activitiesDescription.textContent = data.activities[i][0].description;
                 activitiesImage.style.margin = "15px";
                 activitiesResults.appendChild(activitiesDescription);
-
                 //view URL for activity  - NEEDS URL FROM API ADDED
                 const activitySite = document.createElement('a');
                 activitySite.href = data.activities[i][0].website;
                 activitySite.textContent = data.activities[i][0].name + " website";
                 activitySite.target = "_blank"
                 activitiesResults.appendChild(activitySite);
-
                 //save button to connect to database
                 const activitySaveBtn = document.createElement('button');
-                activitySaveBtn.id = "aSave";
-                activitySaveBtn.className = "save btn btn-primary";
+                activitySaveBtn.className = "aSave save btn btn-primary";
                 activitySaveBtn.innerHTML = "SAVE";
                 activitiesResults.appendChild(activitySaveBtn);
-
-
                 //horizontal rule to seperate results
                 const hr = document.createElement('hr')
                 activitiesResults.appendChild(hr)
             }
         });
     }
-
     function getRestaurantAPI(city) {
         $.get("/api/restaurants", { city: city }).then((data) => {
             console.log(data);
             for (let i = 0; i < data.restaurants.length; i++) {
                 // view restaurant name
                 const restaurantsName = document.createElement("h2");
-                restaurantsName.id = "rName";
+                restaurantsName.className = "rName";
+                restaurantsName.setAttribute('id', `restaurant-${i}`)
                 restaurantsResults.appendChild(restaurantsName);
                 restaurantsName.textContent = data.restaurants[i][0].name;
 
@@ -89,50 +76,37 @@ $(document).ready(() => {
                 restaurantsImage.style.width = "200px";
                 restaurantsImage.style.float = "left";
                 restaurantsImage.style.marginRight = "15px";
-
-
                 document.getElementById("restaurantsResults").appendChild(restaurantsImage);
-
                 //view review for restuarant
                 // const restaurantReview = document.createElement('p');
                 // restaurantReview.textContent = data.restaurant[i][0].reviews[0];
                 // restaurantsResults.appendChild(restaurantReview);
-
                 //view address for restuarant  
                 const restaurantAddress = document.createElement('p');
                 restaurantAddress.textContent = "Address: " + data.restaurants[i][0].address;
                 restaurantsResults.appendChild(restaurantAddress);
-
                 //view phone number for restuarant  
                 const restaurantPhone = document.createElement('p');
                 restaurantPhone.textContent = "Phone: " + data.restaurants[i][0].phone;
                 restaurantsResults.appendChild(restaurantPhone);
-
                 //view URL for restuarant  
                 const restaurantSite = document.createElement('a');
                 restaurantSite.href = data.restaurants[i][0].url;
                 restaurantSite.textContent = data.restaurants[i][0].name + " website";
                 restaurantSite.target = "_blank"
                 restaurantsResults.appendChild(restaurantSite);
-
-
                 // Save button that links to the database
                 const restaurantSaveBtn = document.createElement('button');
-                restaurantSaveBtn.id = "rSave";
-                restaurantSaveBtn.className = "save btn btn-primary";
+                restaurantSaveBtn.className = "rSave save btn btn-primary";
+                restaurantSaveBtn.setAttribute('data-id-target', `${i}`)
                 restaurantSaveBtn.innerHTML = "SAVE";
                 restaurantsResults.appendChild(restaurantSaveBtn);
-
                 //horizontal rule to seperate results
-
                 const hr = document.createElement('hr')
                 restaurantsResults.appendChild(hr)
-
-
             }
         });
     }
-
     $("#create-form").on("submit", event => {
         event.preventDefault();
         const cityName = ca.value.trim();
@@ -142,15 +116,18 @@ $(document).ready(() => {
         $("#ca").val('');
         activitiesResults.innerHTML = '';
         restaurantsResults.innerHTML = '';
-
     });
-
-    $("rSave").on("submit", event => {
+    $(document).on("click", ".rSave", event => {
         event.preventDefault();
+        console.log(event.target.previousElementSibling.textContent)
+        console.log(event.target.previousElementSibling);
+        console.log($(this).siblings('h1'))
+        var restaurantName = document.querySelector(`#restaurant-${event.target.dataset.idTarget}`).textContent
+        console.log(restaurantName)
         const itineraryData = {
-            restaurantName: document.getElementById('rName').value.trim(),
+            restaurantName: restaurantName
         }
-
+        console.log(itineraryData)
         fetch('/api/itinerary', {
             method: 'POST',
             headers: {
@@ -159,23 +136,16 @@ $(document).ready(() => {
             body: JSON.stringify(itineraryData),
         })
     });
-
     // // When the form is submitted, we validate there's an email and password entered
     // searchBtn.on("click", (e) => {
     //   e.preventDefault();
     //   const cityName = ca.val().trim();
     //   console.log("I've been clicked");
     //   console.log(cityName);
-
     // //   // If we have an email and password we run the loginUser function and clear the form
     // //   getRestaurantAPI('New York');
     // //   getActivityResultAPI('New York');
-
     // //   //Empty the search bar once search submitted
     // //   ca.val("");
     // // });
-
-
-
-
 });
