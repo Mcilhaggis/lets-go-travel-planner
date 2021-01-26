396d9f103bdcf5ec5207a57440e7a8cbf9647c8c
Hustin <hustinkava@gmail.com>
2021-01-25 22:30:52 -0700

    conflicts resolved

--- controllers/travelController.js
+++ controllers/travelController.js
@@ -7,6 +7,8 @@ const passport = require("../config/passport");
 // Requiring our models and passport as we've configured it
 const zomato = require("../controllers/zomato");
 const amadeus = require("../controllers/amadeus");
+
+
 //HTML ROUTES
 router.get("/", (req, res) => {
     // If the user already has an account send them to the search page
@@ -16,6 +18,8 @@ router.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname, "../public/signup.html"));
     res.render("signup");
 });
+
+
 router.get("/login", (req, res) => {
     // If the user already has an account send them to the search page
     if (req.user) {
@@ -24,18 +28,24 @@ router.get("/login", (req, res) => {
     // res.sendFile(path.join(__dirname, "../public/login.html"));
     res.render("login");
 });
+
+
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/search", isAuthenticated, (req, res) => {
     // res.sendFile(path.join(__dirname, "../public/search.html"));
     res.render("search");
 });
+
+
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/result", isAuthenticated, (req, res) => {
     // res.sendFile(path.join(__dirname, "../public/result.html"));
     res.render("result");
 });
+
+
 //SIGNUP & LOGIN API ROUTES
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the search page.
@@ -43,6 +53,8 @@ router.get("/result", isAuthenticated, (req, res) => {
 router.post("/api/login", passport.authenticate("local"), (req, res) => {
     res.json(req.user);
 });
+
+
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
@@ -58,11 +70,15 @@ router.post("/api/signup", (req, res) => {
             res.status(401).json(err);
         });
 });
+
+
 // Route for logging user out
 router.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
 });
+
+
 // Route for getting some data about our user to be used client side
 router.get("/api/user_data", (req, res) => {
     if (!req.user) {
@@ -77,6 +93,8 @@ router.get("/api/user_data", (req, res) => {
         });
     }
 });
+
+
 //ITINERARY API ROUTES
 //Route for export
 // GET route for getting all of the saved itenrary items
@@ -88,6 +106,8 @@ router.get("/api/itinerary", (req, res) => {
         },
     }).then((result) => res.json(result));
 });
+
+
 // POST route for saving a new itinerary item
 router.post("/api/itinerary", (req, res) => {
     console.log(req.body);
@@ -95,26 +115,27 @@ router.post("/api/itinerary", (req, res) => {
     // Insert into our table. We pass in an object with a text and complete property.
 
     //If the item is already in there with a matching restaurant name the item will not be created
-     db.Itinerary.findOrCreate({
-        where: {
-            restaurantName: req.body.restaurantName
-        },
-        defaults: {
-        memberId: req.user.id, 
-        destination: req.body.destination,
-        restaurantWebsite: req.body.restaurantWebsite,
-        restaurantName: req.body.restaurantName,
-        restaurantAddress: req.body.restaurantAddress,
-        restaurantPhone: req.body.restaurantPhone,
-        restaurantPhoto: req.body.restaurantPhoto
-        },
-    })
-    .then(async ([result, created]) => {
-    if (!created) {
-        console.log("You added this already")
-    }
+    db.Itinerary.findOrCreate({
+            where: {
+                restaurantName: req.body.restaurantName
+            },
+            defaults: {
+                memberId: req.user.id,
+                destination: req.body.destination,
+                restaurantWebsite: req.body.restaurantWebsite,
+                restaurantName: req.body.restaurantName,
+                restaurantAddress: req.body.restaurantAddress,
+                restaurantPhone: req.body.restaurantPhone,
+                restaurantPhoto: req.body.restaurantPhoto
+            },
+        })
+        .then(async([result, created]) => {
+            if (!created) {
+                console.log("You added this already")
+            }
+        });
 });
-});
+
 
 // Deleting a previously saved item
 router.delete("/api/itinerary/:activityId", (req, res) => {
@@ -129,80 +150,82 @@ router.delete("/api/itinerary/:activityId", (req, res) => {
 const resId = [];
 // Call Api function from Class 'zomato'
 router.get("/api/restaurants", (req, res) => {
-  let allRestaurnt = {}
+    let allRestaurnt = {}
 
-  zomato.getZomatoCityId(req.query.city).then(function(cityId) {
-    zomato.getZomatoRestaurant(cityId).then((result) => {
-      allRestaurnt.restaurants = result.restaurants.map((o) => 
-          (restaurant = {
-            name: o.restaurant.name,
-            url: o.restaurant.url,
-            address: o.restaurant.location.address,
-            menu: o.restaurant.menu_url,
-            phone: o.restaurant.phone_numbers,
-            photos: o.restaurant.featured_image,
-            res_id: o.restaurant.id
-          }),
-        )
-      ;
+    zomato.getZomatoCityId(req.query.city).then(function(cityId) {
+        zomato.getZomatoRestaurant(cityId).then((result) => {
+            allRestaurnt.restaurants = result.restaurants.map((o) =>
+                (restaurant = {
+                    name: o.restaurant.name,
+                    url: o.restaurant.url,
+                    address: o.restaurant.location.address,
+                    menu: o.restaurant.menu_url,
+                    phone: o.restaurant.phone_numbers,
+                    photos: o.restaurant.featured_image,
+                    res_id: o.restaurant.id
+                }),
+            );
 
-   
-      res.json(allRestaurnt);
-      // // ==== PREPARED FOR HANDLE_BAR=====
-      // res.render('search', {allRestaurnt});
+
+            res.json(allRestaurnt);
+            // // ==== PREPARED FOR HANDLE_BAR=====
+            // res.render('search', {allRestaurnt});
+        });
+
+
     });
-
-
-  });
 });
+
 
 // Call 'zomato' Restaurant Reviews
 router.get("/api/restaurantReviews", (req, res) => {
-  zomato.getRestaurantReview(req.query.res_id).then(function(data) {
-      let onlyTwoData = data.user_reviews.slice(0,2);
-      // console.log(onlyTwoData);
-      let allReviews = {
-        reviews: onlyTwoData.map((o) => 
-          (review = {
-            res_id: req.query.res_id,
-            review_text: o.review.review_text,
-            rating_text: o.review.rating_text[0]
+    zomato.getRestaurantReview(req.query.res_id).then(function(data) {
+        let onlyTwoData = data.user_reviews.slice(0, 2);
+        // console.log(onlyTwoData);
+        let allReviews = {
+            reviews: onlyTwoData.map((o) =>
+                (review = {
+                    res_id: req.query.res_id,
+                    review_text: o.review.review_text,
+                    rating_text: o.review.rating_text[0]
 
-          }),
-        ),
-      };
+                }),
+            ),
+        };
 
-      res.json(allReviews);
-  });
+        res.json(allReviews);
+    });
 });
+
 
 // Get Activities from Amadeus API
 router.get("/api/activity", (req, res) => {
-  //Get geo-location
-  amadeus.getActivity(req.query.city).then(function(geocode) {
-    // Get amadeus token
-    amadeus.getTokenActivities().then(function(token) {
-      // Get amadeus Activities
-      amadeus.getActivityResult(token, geocode).then(function(activities) {
-        let act = activities.data.slice(0, 3);
-        // console.log(act);
-        let allActivities = {
-          activities: act.map((o) => [
-            (Activity = {
-              name: o.name,
-              description: o.shortDescription,
-              rating: o.rating,
-              price: o.price.amount,
-              photo: o.pictures[0],
-            }),
-          ]),
-        };
-        
-        res.send(allActivities);
+    //Get geo-location
+    amadeus.getActivity(req.query.city).then(function(geocode) {
+        // Get amadeus token
+        amadeus.getTokenActivities().then(function(token) {
+            // Get amadeus Activities
+            amadeus.getActivityResult(token, geocode).then(function(activities) {
+                let act = activities.data.slice(0, 3);
+                // console.log(act);
+                let allActivities = {
+                    activities: act.map((o) => [
+                        (Activity = {
+                            name: o.name,
+                            description: o.shortDescription,
+                            rating: o.rating,
+                            price: o.price.amount,
+                            photo: o.pictures[0],
+                        }),
+                    ]),
+                };
 
-        // // ==== PREPARED FOR HANDLEBARS=====
-        // res.render('result', {allActivities});
-      });
+                res.send(allActivities);
+
+                // // ==== PREPARED FOR HANDLEBARS=====
+                // res.render('result', {allActivities});
+            });
+        });
     });
 });
 // Export routes for server.js to use.
